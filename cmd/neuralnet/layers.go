d67179b299551b54b95bf86b01015f3a1cdc8f93
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-04-19 18:32:28 +0200

    define functions for Layer interface

--- cmd/neuralnet/layers.go
+++ cmd/neuralnet/layers.go
@@ -2,6 +2,8 @@ package neuralnet
 
 // default methods that have to be implemented by layers
 type Layer interface {
+	forward(input float64) float64
+	backward(outputGradient []float64, learning_rate float64) []float64
 }
 
 type BaseLayer struct {
