2872afa8780ece4c4e2a4a8972f2613518773918
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-05-01 19:23:38 +0200

    implement basic denselayer

--- cmd/neuralnet/layers.go
+++ cmd/neuralnet/layers.go
@@ -1,5 +1,9 @@
 package neuralnet
 
+import (
+	"gonum.org/v1/gonum/floats"
+)
+
 // default methods that have to be implemented by layers
 type Layer interface {
 	forward(input float64) float64
@@ -7,6 +11,22 @@ type Layer interface {
 }
 
 type BaseLayer struct {
-	input  float64
-	output float64
+	input  []float64
+	output []float64
+}
+
+type DenseLayer struct {
+	base    BaseLayer
+	weights []float64
+	bias    float64
+}
+
+func (d DenseLayer) forward(input []float64) float64 {
+	d.base.input = input
+	return floats.Dot(d.weights, input)
+}
+
+func (d DenseLayer) backward(outputGradient []float64, learning_rate float64) []float64 {
+
+	return nil
 }
