f27f1520147d0bf696035763c2ff470db72cbac4
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-05-02 19:17:00 +0200

    using gonum matrix and vector datatypes

--- cmd/neuralnet/layers.go
+++ cmd/neuralnet/layers.go
@@ -1,7 +1,7 @@
 package neuralnet
 
 import (
-	"gonum.org/v1/gonum/floats"
+	"gonum.org/v1/gonum/mat"
 )
 
 // default methods that have to be implemented by layers
@@ -11,19 +11,26 @@ type Layer interface {
 }
 
 type BaseLayer struct {
-	input  []float64
-	output []float64
+	input  mat.VecDense
+	output mat.VecDense
 }
 
 type DenseLayer struct {
 	base    BaseLayer
-	weights []float64
-	bias    float64
+	weights mat.Dense
+	bias    mat.VecDense
 }
 
-func (d DenseLayer) forward(input []float64) float64 {
+// constructor for DenseLayer
+func NewDenseLayer(inputSize int, outputSize int) DenseLayer {
+
+}
+
+func (d DenseLayer) forward(input mat.VecDense) mat.VecDense {
 	d.base.input = input
-	return floats.Dot(d.weights, input)
+	var ans *mat.VecDense
+	ans.MulVec(&d.weights, &input)
+	return *ans
 }
 
 func (d DenseLayer) backward(outputGradient []float64, learning_rate float64) []float64 {
