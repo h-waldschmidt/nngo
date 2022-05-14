75903f4e35ada64383b5910b3c8ffb522ab1c307
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-05-14 20:07:16 +0200

    sigmoid and derivative added

---
+++ cmd/neuralnet/activationfunctions.go
@@ -0,0 +1,12 @@
+package neuralnet
+
+import "math"
+
+func Sigmoid(x float64) float64 {
+	return 1 / (1 + math.Exp(-x))
+}
+
+func SigmoidDerivative(x float64) float64 {
+	s := Sigmoid(x)
+	return s * (1 - s)
+}
