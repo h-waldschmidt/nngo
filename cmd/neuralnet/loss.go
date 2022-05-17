1e3cf51fbdcffc64e4af68ad9187355cd699bf38
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-05-17 18:19:21 +0200

    added mae loss

--- cmd/neuralnet/loss.go
+++ cmd/neuralnet/loss.go
@@ -18,3 +18,16 @@ func Mse(yTrue, yPred mat.VecDense) float64 {
 
 	return sum / float64(yTrue.Len())
 }
+
+func Mae(yTrue, yPred mat.VecDense) float64 {
+	if yTrue.Len() != yPred.Len() {
+		panic("vectors need to have the same dimesions")
+	}
+
+	var sum float64
+	for i := 0; i < yTrue.Len(); i++ {
+		sum += math.Abs(yTrue.AtVec(i) - yPred.AtVec(i))
+	}
+
+	return sum / float64(yTrue.Len())
+}
