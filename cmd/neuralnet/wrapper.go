189466e4aa4c695e1a934d3aa9e9ee8ea706bbac
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-08-05 15:49:14 +0200

    make function public

--- cmd/neuralnet/wrapper.go
+++ cmd/neuralnet/wrapper.go
@@ -5,7 +5,7 @@ import "gonum.org/v1/gonum/mat"
 // provides function to that make accessability of the gonum.mat functions more accessible
 
 // return the column as a vector instead of an slice
-func getColVector(matrix *mat.Dense, index int) mat.VecDense {
+func GetColVector(matrix *mat.Dense, index int) mat.VecDense {
 	var col []float64
 	col = mat.Col(col, index, matrix)
 	vector := mat.NewVecDense(len(col), col)
