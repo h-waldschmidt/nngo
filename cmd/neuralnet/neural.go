a95f43a613ec8e9dc67600944546116ab7e1854d
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-08-05 15:46:39 +0200

    add function for creating new Set

--- cmd/neuralnet/neural.go
+++ cmd/neuralnet/neural.go
@@ -1,18 +1,38 @@
 package neural
 
+import "gonum.org/v1/gonum/mat"
+
 // the used data consists of float vectors
 // this achieves maximum flexibility
 // users can use one hot encoding, one dimensional vectors and n-dimensional vectors
 
 // you can read the complete dataset and then later convert it into splitSet with splitDataSet()
 type CompleteSet struct {
-	data [][]float64
+	data mat.Dense
 }
 
 // saves train and test data into struct
 type SplitSet struct {
-	trainData [][]float64
-	testData  [][]float64
+	trainData mat.Dense
+	testData  mat.Dense
+}
+
+func NewCompleteSet(data [][]float64) *CompleteSet {
+	var completeSet *CompleteSet
+
+	return completeSet
+}
+
+func NewSplitSet(testData, trainData [][]float64) *SplitSet {
+	var splitSet *SplitSet
+
+	return splitSet
+}
+
+func newSplitSet(data [][]float64, splitRation float64) *SplitSet {
+	var splitSet *SplitSet
+
+	return splitSet
 }
 
 func (set *CompleteSet) splitDataSet() (SplitSet, error) {
@@ -27,7 +47,7 @@ type Network struct {
 	lossFuncDerivative lossFuncDerivative
 }
 
-func (dense *Network) predict() {}
+func (dense *Network) predict(input mat.VecDense) {}
 
 func (dense *Network) train() {}
 
