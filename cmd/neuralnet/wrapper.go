6e3b6a708777fc09d0db9422bfbdf50cc2e2ff0e
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2022-08-12 10:30:31 +0200

    add getMaxIndex

--- cmd/neuralnet/wrapper.go
+++ cmd/neuralnet/wrapper.go
@@ -1,6 +1,10 @@
 package neural
 
-import "gonum.org/v1/gonum/mat"
+import (
+	"math"
+
+	"gonum.org/v1/gonum/mat"
+)
 
 // provides function to that make accessability of the gonum.mat functions more accessible
 
@@ -10,4 +14,19 @@ func GetColVector(matrix *mat.Dense, index int) mat.VecDense {
 	col = mat.Col(col, index, matrix)
 	vector := mat.NewVecDense(len(col), col)
 	return *vector
+}
+
+// returns the index of the element with the max value
+func GetMaxIndex(vector *mat.VecDense) int {
+	maxValue := math.Inf(-1)
+	maxIndex := -1
+	for i := 0; i < vector.Len(); i++ {
+		value := vector.AtVec(i)
+		if value > maxValue {
+			maxValue = value
+			maxIndex = i
+		}
+	}
+
+	return maxIndex
 }
